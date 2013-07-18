5ae1b559747cbd59dc2e8992cae2e8a86bdcee01
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-07-17 21:35:49 -0400

    Make time 64 bits

--- globals.v
+++ globals.v
@@ -4,16 +4,16 @@ timeprecision 1ps;
 package CodelPkg;
 
 localparam  MAX_PACKET          =   8;      // Total size in bytes / Packet size in bytes
-localparam  TARGET              =   32'b00000000000000001001010101000111;
-localparam  NUM_COUNT           =   (1'b1<<32)-1;
-localparam  TIME_COUNTER_LENGTH =   32;
-localparam  INTERVAL            =   32'b00000000000000001001010101000111;
+localparam  TARGET              =   64'b00000000000000001001010101000111;
+localparam  NUM_COUNT           =   (1'b1<<64)-1;
+localparam  TIME_COUNTER_LENGTH =   64;
+localparam  INTERVAL            =   64'b00000000000000001001010101000111;
 
 localparam  PACKET_DATA_WIDTH   =   10;
 localparam  QUEUE_DEPTH         =   2;
 
-typedef logic   [31:0]                      TimeCtr;
-typedef logic   [31:0]                      Count;
+typedef logic   [63:0]                      TimeCtr;
+typedef logic   [9:0]                       Count;
 typedef logic   [$clog2(QUEUE_DEPTH)-1:0]   QueueLength;
 
 typedef struct packed {
