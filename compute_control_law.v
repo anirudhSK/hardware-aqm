c19676870152de06ebb9e431509c1c4af7675689
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-07-17 21:36:51 -0400

    Use a real table lookup implementation for square root.

--- compute_control_law.v
+++ compute_control_law.v
@@ -15,8 +15,18 @@ input   Count                   i__count;
 /*** Outputs                    ***/
 output  TimeCtr                 o__output;
 
+/*** Wire Nets                  ***/
+TimeCtr                         w__invsqrt;
+
+/*** Sub-modules                ***/
+invsqrt get_invsqrt             (
+        .i__input               (i__count),
+        .o__output              (w__invsqrt)
+);
+
 /*** Combinational Logic        ***/
-assign  o__output   = (i__input + (i__interval / i__count));  // TODO Wrong formula, but okay for now
+// assign  o__output   = (i__input + (i__interval / i__count));  // TODO Wrong formula, but okay for now
+assign  o__output   = (i__input + w__invsqrt); 
 
 endmodule
 
